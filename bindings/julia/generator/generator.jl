887290186606d13b39d73a37ade6d539d97b058b
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-09 12:01:23 -0300

    [skip ci] update

--- bindings/julia/generator/generator.jl
+++ bindings/julia/generator/generator.jl
@@ -12,8 +12,7 @@ include_dir = joinpath(database_dir, "include", "psr", "c")
 
 Libdl.dlopen(joinpath(database_dir, "build", "bin", "libpsr_database_c.dll"))
 
-@show readdir(include_dir)
-@show headers = [joinpath(include_dir, header) for header in readdir(include_dir) if endswith(header, ".h") && header != "platform.h"]
+headers = [joinpath(include_dir, header) for header in readdir(include_dir) if endswith(header, ".h") && header != "platform.h"]
 args = get_default_args()
 options = load_options(joinpath(@__DIR__, "generator.toml"))
 ctx = create_context(headers, args, options)
