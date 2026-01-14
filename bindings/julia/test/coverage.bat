420c622fe3676b8d8041c9880d9d4de9cbc8bd60
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-14 16:58:36 -0300

    update

--- bindings/julia/test/coverage.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-SET BASE_PATH=%~dp0
-
-CALL julia +1.12.3 --project=%BASE_PATH%\.. --code-coverage=user -e "import Pkg; Pkg.test()"
-CALL julia +1.12.3 --project=%BASE_PATH%\.. %BASE_PATH%coverage.jl
--- bindings/julia/test/coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export LD_LIBRARY_PATH="$BASEPATH/../../../build/lib:$LD_LIBRARY_PATH"
-
-julia --project=$BASEPATH/.. --code-coverage=user -e "import Pkg; Pkg.test()"
-julia --project=$BASEPATH/.. $BASEPATH/coverage.jl
