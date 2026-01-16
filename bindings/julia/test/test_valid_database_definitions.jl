5a4c2651ffc96c56cf8b2471a20b9849edd84bae
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-16 10:50:29 -0300

    update

--- bindings/julia/test/test_valid_database_definitions.jl
+++ bindings/julia/test/test_valid_database_definitions.jl
@@ -28,65 +28,47 @@ end
 
 @testset "Invalid - No Configuration" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "no_configuration.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - Label Not Null" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "label_not_null.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - Label Not Unique" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "label_not_unique.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - Label Wrong Type" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "label_wrong_type.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - Duplicate Attribute" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "duplicate_attribute.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - Vector No Index" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "vector_no_index.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - Set No Unique" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "set_no_unique.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - FK Not Null Set Null" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "fk_not_null_set_null.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 @testset "Invalid - FK Actions" begin
     path_schema = joinpath(tests_path(), "schemas", "invalid", "fk_actions.sql")
-    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(
-        ":memory:",
-        path_schema,)
+    @test_throws PSRDatabase.DatabaseException PSRDatabase.from_schema(":memory:", path_schema)
 end
 
 end
