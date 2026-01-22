6400376a951af0dbfab52601abba374441dea6cd
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-22 17:29:17 -0300

    update

--- bindings/dart/test/test.bat
+++ bindings/dart/test/test.bat
@@ -1,11 +1,4 @@
 @echo off
-
-SET BASE_PATH=%~dp0..
-SET LIB_PATH=%BASE_PATH%\..\..\build\bin
-
-REM Add library path to PATH so Windows can find DLL dependencies
-SET PATH=%LIB_PATH%;%PATH%
-
-pushd %BASE_PATH%
+pushd %~dp0..
 dart test %*
 popd
--- bindings/dart/test/test.sh
+++ bindings/dart/test/test.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-export LD_LIBRARY_PATH="$BASEPATH/../../build/lib:$LD_LIBRARY_PATH"
-
-cd "$BASEPATH"
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 dart test "$@"
