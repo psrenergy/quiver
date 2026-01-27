f670328a48fb00fd12453c5ef0e25457ba9c24ba
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-01-26 23:32:17 -0300

    update

--- bindings/dart/lib/quiver_db.dart
+++ bindings/dart/lib/quiver_db.dart
@@ -1,14 +1,7 @@
 library quiver;
 
 export 'src/database.dart'
-    show
-        Database,
-        DatabaseCreate,
-        DatabaseDelete,
-        DatabaseMetadata,
-        DatabaseRead,
-        DatabaseRelations,
-        DatabaseUpdate;
+    show Database, DatabaseCreate, DatabaseDelete, DatabaseMetadata, DatabaseRead, DatabaseRelations, DatabaseUpdate;
 export 'src/date_time.dart' show dateTimeToString, stringToDateTime;
 export 'src/element.dart' show Element;
 export 'src/exceptions.dart';
