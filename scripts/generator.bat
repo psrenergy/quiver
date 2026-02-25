818f637182d81bb8d73d24776d5c31b56343e4ed
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-02-25 15:40:06 -0300

    Update

--- scripts/generator.bat
+++ scripts/generator.bat
@@ -23,4 +23,14 @@ if %ERRORLEVEL% neq 0 (
 )
 
 echo.
+echo Generating Python FFI bindings...
+pushd "%ROOT%\bindings\python\generator"
+call generator.bat
+popd
+if %ERRORLEVEL% neq 0 (
+    echo Failed to generate Python FFI bindings
+    exit /b 1
+)
+
+echo.
 echo All FFI bindings generated.
