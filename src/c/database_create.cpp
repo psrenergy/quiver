a29824007a1efdd3ac1dd70740114544a1597433
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-02-25 17:47:41 -0300

    Update

--- src/c/database_create.cpp
+++ src/c/database_create.cpp
@@ -19,16 +19,4 @@ QUIVER_C_API quiver_error_t quiver_database_create_element(quiver_database_t* db
     }
 }
 
-QUIVER_C_API quiver_error_t quiver_database_delete_element(quiver_database_t* db, const char* collection, int64_t id) {
-    QUIVER_REQUIRE(db, collection);
-
-    try {
-        db->db.delete_element(collection, id);
-        return QUIVER_OK;
-    } catch (const std::exception& e) {
-        quiver_set_last_error(e.what());
-        return QUIVER_ERROR;
-    }
-}
-
 }  // extern "C"
