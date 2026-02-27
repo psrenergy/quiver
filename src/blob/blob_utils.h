0fec191b4704ece2e591a0a5979078a3a6304948
gvidigal-psr <gvidigal@psr-inc.com>
2026-02-27 14:53:05 -0300

    format

--- src/blob/blob_utils.h
+++ src/blob/blob_utils.h
@@ -1,15 +1,17 @@
 #ifndef QUIVER_BLOB_UTILS_H
 #define QUIVER_BLOB_UTILS_H
 
+#include "quiver/blob/time_constants.h"
+
 #include <chrono>
 #include <cstdint>
 #include <string_view>
 
 namespace quiver {
 
-constexpr std::string_view QVR_EXTENSION  = ".qvr";
+constexpr std::string_view QVR_EXTENSION = ".qvr";
 constexpr std::string_view TOML_EXTENSION = ".toml";
-constexpr std::string_view CSV_EXTENSION  = ".csv";
+constexpr std::string_view CSV_EXTENSION = ".csv";
 
 namespace chrono = std::chrono;
 
@@ -22,11 +24,9 @@ inline int64_t day_of_year(chrono::system_clock::time_point datetime) {
 
 inline int64_t day_of_week(chrono::system_clock::time_point datetime) {
     int day_of_year = quiver::day_of_year(datetime);
-    return (day_of_year - 1) % quiver::time::MAX_DAYS_IN_WEEK + 1; // 1-7 instead of 0-6
+    return (day_of_year - 1) % quiver::time::MAX_DAYS_IN_WEEK + 1;  // 1-7 instead of 0-6
 }
 
 }  // namespace quiver
-
-
 
 #endif  // QUIVER_BLOB_UTILS_H
