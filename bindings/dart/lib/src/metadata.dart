912072cc5c2a1467583228718f512571fdb75ab2
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-03-01 22:32:13 -0300

    Update

--- bindings/dart/lib/src/metadata.dart
+++ bindings/dart/lib/src/metadata.dart
@@ -28,20 +28,18 @@ class ScalarMetadata {
 
   /// Creates a ScalarMetadata from a native quiver_scalar_metadata_t struct.
   ScalarMetadata.fromNative(quiver_scalar_metadata_t native)
-      : name = native.name.cast<Utf8>().toDartString(),
-        dataType = native.data_type,
-        notNull = native.not_null != 0,
-        primaryKey = native.primary_key != 0,
-        defaultValue = native.default_value == nullptr
-            ? null
-            : native.default_value.cast<Utf8>().toDartString(),
-        isForeignKey = native.is_foreign_key != 0,
-        referencesCollection = native.references_collection == nullptr
-            ? null
-            : native.references_collection.cast<Utf8>().toDartString(),
-        referencesColumn = native.references_column == nullptr
-            ? null
-            : native.references_column.cast<Utf8>().toDartString();
+    : name = native.name.cast<Utf8>().toDartString(),
+      dataType = native.data_type,
+      notNull = native.not_null != 0,
+      primaryKey = native.primary_key != 0,
+      defaultValue = native.default_value == nullptr ? null : native.default_value.cast<Utf8>().toDartString(),
+      isForeignKey = native.is_foreign_key != 0,
+      referencesCollection = native.references_collection == nullptr
+          ? null
+          : native.references_collection.cast<Utf8>().toDartString(),
+      referencesColumn = native.references_column == nullptr
+          ? null
+          : native.references_column.cast<Utf8>().toDartString();
 }
 
 /// Metadata for a vector, set, or time series group in a collection.
@@ -58,12 +56,10 @@ class GroupMetadata {
 
   /// Creates a GroupMetadata from a native quiver_group_metadata_t struct.
   GroupMetadata.fromNative(quiver_group_metadata_t native)
-      : groupName = native.group_name.cast<Utf8>().toDartString(),
-        dimensionColumn = native.dimension_column == nullptr
-            ? ''
-            : native.dimension_column.cast<Utf8>().toDartString(),
-        valueColumns = List.generate(
-          native.value_column_count,
-          (i) => ScalarMetadata.fromNative(native.value_columns[i]),
-        );
+    : groupName = native.group_name.cast<Utf8>().toDartString(),
+      dimensionColumn = native.dimension_column == nullptr ? '' : native.dimension_column.cast<Utf8>().toDartString(),
+      valueColumns = List.generate(
+        native.value_column_count,
+        (i) => ScalarMetadata.fromNative(native.value_columns[i]),
+      );
 }
