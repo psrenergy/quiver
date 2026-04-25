c65966b3cc755b1864e8f08bf2d50f204c3a9803
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-04-25 03:57:13 -0300

    update

--- tests/test_issues.cpp
+++ tests/test_issues.cpp
@@ -8,7 +8,11 @@ namespace fs = std::filesystem;
 
 class IssuesFixture : public ::testing::Test {
 protected:
-    void SetUp() override { issues_path = (fs::path(__FILE__).parent_path() / "schemas" / "issues").string(); }
+    void SetUp() override { 
+        schemas_path = (fs::path(__FILE__).parent_path() / "schemas").string();
+        issues_path = (fs::path(__FILE__).parent_path() / "schemas" / "issues").string();
+    }
+    std::string schemas_path;
     std::string issues_path;
 };
 
@@ -19,12 +23,11 @@ TEST_F(IssuesFixture, Issue52) {
 }
 
 TEST_F(IssuesFixture, Issue161) {
-	auto migrations_path = issues_path + "/issue161";
-    auto schema_path = issues_path + "/issue161/schema.sql";
-    auto db_path = (fs::temp_directory_path() / "issue161.db").string();
+	auto migrations_path = schemas_path + "/migrations";
+    auto schema_path = schemas_path + "/valid/basic.sql";
 
     quiver::DatabaseOptions options;
     options.read_only = true;
-    EXPECT_THROW(quiver::Database::from_migrations(db_path, migrations_path, options), std::runtime_error);
-    EXPECT_THROW(quiver::Database::from_schema(db_path, schema_path, options), std::runtime_error);
+    EXPECT_THROW(quiver::Database::from_migrations(":memory:", migrations_path, options), std::runtime_error);
+    EXPECT_THROW(quiver::Database::from_schema(":memory:", schema_path, options), std::runtime_error);
 }
