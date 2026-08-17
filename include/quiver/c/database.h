ffd7c33b9064ef1c44b29386410c2bce135e93ee
Raphael Sampaio <raphaelaraujosampaio@gmail.com>
2026-08-17 15:22:05 -0300

    update

--- include/quiver/c/database.h
+++ include/quiver/c/database.h
@@ -67,7 +67,6 @@ QUIVER_C_API quiver_error_t quiver_database_update_element(quiver_database_t* db
 
 QUIVER_C_API quiver_error_t quiver_database_delete_element(quiver_database_t* db, const char* collection, int64_t id);
 
-
 QUIVER_C_API quiver_error_t quiver_database_delete_element_by_label(quiver_database_t* db,
                                                                     const char* collection,
                                                                     const char* label);
